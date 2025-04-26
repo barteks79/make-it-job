cd1f19e202fd77d5466c7cfd8dfc9fa43406cc40
Bartosz Sarnowski <sade79dev@gmail.com>
2025-04-26 19:20:48 +0200

    feat: add styles to navigation component

--- src/components/custom/navigation.tsx
+++ src/components/custom/navigation.tsx
@@ -4,12 +4,15 @@ import { Inbox, Plus } from 'lucide-react';
 
 export default function Navigation() {
 	return (
-		<nav>
-			<div>
+		<nav className="flex justify-between items-center border-b px-8 h-16 lg:h-20">
+			<div className="flex items-center gap-5 lg:gap-12">
 				{/* hamburger menu  */}
-				<h1>MakeITWork</h1>
 
-				<ul>
+				<h1 className="font-flavors text-sm md:text-base lg:text-lg tracking-wider uppercase text-foreground lg:text-secondary-foreground">
+					MakeITWork
+				</h1>
+
+				<ul className="hidden lg:flex gap-6 text-sm">
 					<NavItem href="/dashboard">Dashboard</NavItem>
 					<NavItem href="/offers">Offers</NavItem>
 					<NavItem href="/bookmarks">Bookmarks</NavItem>
@@ -17,7 +20,7 @@ export default function Navigation() {
 				</ul>
 			</div>
 
-			<ul>
+			<ul className="flex gap-3">
 				<Button variant="outline" size="icon">
 					<Plus />
 				</Button>
