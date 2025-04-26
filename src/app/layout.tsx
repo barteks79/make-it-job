2afb7ee1c8c33cdd249cfc2f2967274e0050ac4c
Bartosz Sarnowski <sade79dev@gmail.com>
2025-04-26 15:02:50 +0200

    feat: implement theme provider inside the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,23 @@
-import { type PropsWithChildren } from 'react';
+import { ThemeProvider } from '@/store/theme-provider';
 import { LAYOUT_METADATA } from '@/config/metadata';
+import { type PropsWithChildren } from 'react';
 import './globals.css';
 
 export const metadata = LAYOUT_METADATA.root;
 
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
-		<html lang="en">
-			<body>{children}</body>
+		<html lang="en" suppressHydrationWarning>
+			<body>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
