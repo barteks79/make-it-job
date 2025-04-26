08f7f1582148dd6c0838b8209f52cbd6e8b6c013
Bartosz Sarnowski <sade79dev@gmail.com>
2025-04-26 18:56:41 +0200

    feat: create navigation with no styles yet

--- src/components/custom/navigation.tsx
+++ src/components/custom/navigation.tsx
@@ -1,1 +1,33 @@
-export default function Navigation() {}
+import { Button } from '@/components/ui/button';
+import NavItem from '@/components/custom/nav-item';
+import { Inbox, Plus } from 'lucide-react';
+
+export default function Navigation() {
+	return (
+		<nav>
+			<div>
+				{/* hamburger menu  */}
+				<h1>MakeITWork</h1>
+			</div>
+
+			<ul>
+				<NavItem href="/dashboard">Dashboard</NavItem>
+				<NavItem href="/offers">Offers</NavItem>
+				<NavItem href="/bookmarks">Bookmarks</NavItem>
+				<NavItem href="/pricing">Pricing</NavItem>
+			</ul>
+
+			<ul>
+				<Button variant="outline" size="icon">
+					<Plus />
+				</Button>
+
+				<Button variant="outline" size="icon">
+					<Inbox />
+				</Button>
+
+				{/* dropdown with users profile picture */}
+			</ul>
+		</nav>
+	);
+}
