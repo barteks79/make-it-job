0f799230beb0e444356867f71d46418d2540b458
Bartosz Sarnowski <sade79dev@gmail.com>
2025-04-26 18:18:29 +0200

    feat: install geist package, use it in root layout; add flavors font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,24 @@
 import { ThemeProvider } from '@/store/theme-provider';
 import { LAYOUT_METADATA } from '@/config/metadata';
+
+import { GeistSans } from 'geist/font/sans';
+import { Flavors } from 'next/font/google';
+
 import { type PropsWithChildren } from 'react';
 import './globals.css';
 
 export const metadata = LAYOUT_METADATA.root;
 
+const flavors = Flavors({ weight: '400', variable: '--font-flavors' });
+
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<body>
+		<html
+			lang="en"
+			className={`${GeistSans.variable} ${flavors.variable}`}
+			suppressHydrationWarning
+		>
+			<body className={`${GeistSans.className}`}>
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
