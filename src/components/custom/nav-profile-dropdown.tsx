e45ce3ae832e4ccfc7ef90745851a467ca7a4c67
Bartosz Sarnowski <sade79dev@gmail.com>
2025-05-01 19:33:33 +0200

    feat: create upper section of the profile dropdown

--- src/components/custom/nav-profile-dropdown.tsx
+++ src/components/custom/nav-profile-dropdown.tsx
@@ -4,20 +4,48 @@ import { ChevronsUpDownIcon } from 'lucide-react';
 import {
 	DropdownMenu,
 	DropdownMenuContent,
-	DropdownMenuTrigger
+	DropdownMenuTrigger,
+	DropdownMenuLabel
 } from '@/components/ui/dropdown-menu';
+
+import {
+	Avatar,
+	AvatarImage,
+	AvatarFallback
+} from '@/components/ui/avatar';
 
 export default function NavProfileDropdown() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="outline" className="h-8">
+				<Button variant="outline" className="h-8 gap-6">
 					<span>noahw_</span>
 					<ChevronsUpDownIcon />
 				</Button>
 			</DropdownMenuTrigger>
 
-			<DropdownMenuContent></DropdownMenuContent>
+			<DropdownMenuContent className="min-w-60" align="end" sideOffset={6}>
+				<DropdownMenuLabel className="p-0 font-normal">
+					<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+						<Avatar className="w-10 h-10 rounded-lg">
+							<AvatarImage
+								className="object-cover object-center"
+								src="/mock-profile-picture.jpg"
+							/>
+							<AvatarFallback className="rounded-lg">
+								Profile picture
+							</AvatarFallback>
+						</Avatar>
+
+						<div className="grid flex-1 text-left text-sm leading-tight">
+							<span className="truncate font-semibold">Noah Williams</span>
+							<span className="truncate text-xs">
+								noah.williams@gmail.com
+							</span>
+						</div>
+					</div>
+				</DropdownMenuLabel>
+			</DropdownMenuContent>
 		</DropdownMenu>
 	);
 }
