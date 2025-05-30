de6a43f30b05a822710c63014cdbf830cd8cedc4
Bartosz Sarnowski <sade79dev@gmail.com>
2025-05-30 21:34:34 +0200

    feat: add CardContent with job description and experience, salary, work type icons

--- src/app/(core)/offers/_components/job-post-card.tsx
+++ src/app/(core)/offers/_components/job-post-card.tsx
@@ -1,11 +1,15 @@
+import { type PropsWithChildren } from 'react';
 import { Button } from '@/components/ui/button';
-import { BookmarkIcon } from 'lucide-react';
-import Image from 'next/image';
+import { BookmarkIcon, DollarSignIcon, MonitorIcon } from 'lucide-react';
+import { Badge }
+
 import {
 	Avatar,
 	AvatarImage,
 	AvatarFallback
 } from '@/components/ui/avatar';
+
+import { UserIcon } from 'lucide-react';
 
 import {
 	Card,
@@ -18,7 +22,7 @@ import {
 
 export default function JobPostCard() {
 	return (
-		<Card className="py-4">
+		<Card className="py-4 gap-4">
 			<CardHeader className="flex flex-col gap-2">
 				<div className="flex justify-between items-center w-full">
 					<p className="text-sm text-muted-foreground">3 days ago</p>
@@ -44,6 +48,30 @@ export default function JobPostCard() {
 					</figcaption>
 				</figure>
 			</CardHeader>
+
+			<CardContent className="flex flex-col gap-4">
+				<ul className="flex justify-between items-center">
+					<li className="flex items-center gap-2">
+						<UserIcon className="size-4 text-secondary-foreground" />
+						<p className="text-sm">Senior</p>
+					</li>
+
+					<li className="flex items-center gap-1">
+						<DollarSignIcon className="size-4 text-secondary-foreground" />
+						<p className="text-sm">120.000</p>
+					</li>
+
+					<li className="flex items-center gap-1">
+						<MonitorIcon className="size-4 text-secondary-foreground" />
+						<p className="text-sm">Full time</p>
+					</li>
+				</ul>
+
+				<p className="text-muted-foreground text-sm border-b pb-4">
+					Build stunning, interactive interfaces that redefine the
+					streaming experience for millions.
+				</p>
+			</CardContent>
 		</Card>
 	);
 }
