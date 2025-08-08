ddefe750c83f37103d611fc2eead55f189f7a90c
bartekvs08_ <sade79dev@gmail.com>
2025-08-08 23:00:00 +0200

    feat: create reusable QuickInfoItem component

--- src/app/(core)/offers/@details/JobDetails.tsx
+++ src/app/(core)/offers/@details/JobDetails.tsx
@@ -1,81 +1,72 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { MapPin, DollarSignIcon, MonitorIcon, UserIcon, HouseIcon } from 'lucide-react';
+import { MapPin, DollarSignIcon, MonitorIcon, UserIcon, HouseIcon, Calendar } from 'lucide-react';
 import { type JobPost } from '@/types/job-post';
+import type { ComponentType, SVGProps } from 'react';
 
 export default function JobDetails({ post }: { post: JobPost }) {
   return (
     <>
-      <header className="flex items-center gap-2.5 py-5 border-b">
-        <Avatar className="size-12 rounded">
+      <header className="flex items-center gap-5 py-5 border-b">
+        <Avatar className="size-14 rounded">
           <AvatarImage src={post.image} alt={`${post.company} Logo`} />
           <AvatarFallback>{post.company} Logo</AvatarFallback>
         </Avatar>
 
         <div className="flex flex-col">
-          <h2 className="text-sm font-medium">
+          <h2 className="tracking-tight font-medium">
             {post.title}, {post.company}
           </h2>
-          <p className="text-xs text-accent-foreground">{post.description}</p>
+          <p className="text-sm text-accent-foreground">{post.description}</p>
         </div>
       </header>
 
-      <section className="grid grid-cols-2 gap-6 place-content-between p-5 border-b">
-        <div className="flex items-center gap-2">
-          <MonitorIcon strokeWidth={1.2} className="size-10 text-secondary-foreground" />
-          <div className="flex flex-col">
-            <label className="text-sm text-accent-foreground">Job Type</label>
-            <p className="-mt-1 font-medium">{post.type}</p>
-          </div>
-        </div>
+      <section className="grid grid-cols-2 gap-y-6 place-content-between py-5 px-3 border-b">
+        <QuickInfoItem icon={MonitorIcon} label="Job Type" info={post.type} />
 
-        <div className="flex items-center gap-2">
-          <HouseIcon strokeWidth={1.2} className="size-10 text-secondary-foreground" />
-          <div className="flex flex-col">
-            <label className="text-sm text-accent-foreground">Work Type</label>
-            <p className="-mt-1 font-medium">{post.work}</p>
-          </div>
-        </div>
+        <QuickInfoItem icon={HouseIcon} label="Work Type" info={post.work} />
 
-        <div className="flex items-center gap-2">
-          <DollarSignIcon strokeWidth={1.2} className="size-10 text-secondary-foreground" />
-          <div className="flex flex-col">
-            <label className="text-sm text-accent-foreground">Annual Salary</label>
-            <p className="-mt-1 font-medium">
-              {new Intl.NumberFormat('us-US', {
-                style: 'currency',
-                currency: 'USD',
-                maximumFractionDigits: 0
-              }).format(post.salary)}
-            </p>
-          </div>
-        </div>
+        <QuickInfoItem
+          icon={DollarSignIcon}
+          label="Annual Salary"
+          info={new Intl.NumberFormat('us-US', {
+            style: 'currency',
+            currency: 'USD',
+            maximumFractionDigits: 0
+          }).format(post.salary)}
+        />
 
-        <div className="flex items-center gap-2">
-          <UserIcon strokeWidth={1.2} className="size-10 text-secondary-foreground" />
-          <div className="flex flex-col">
-            <label className="text-sm text-accent-foreground">Experience</label>
-            <p className="-mt-1 font-medium">{post.experience}</p>
-          </div>
-        </div>
+        <QuickInfoItem icon={UserIcon} label="Experience" info={post.experience} />
 
-        <div className="flex items-center gap-2">
-          <MapPin strokeWidth={1.2} className="size-10 text-secondary-foreground" />
-          <div className="flex flex-col">
-            <label className="text-sm text-accent-foreground">Location</label>
-            <p className="-mt-1 font-medium">Toronto, Canada</p>
-          </div>
-        </div>
+        <QuickInfoItem icon={MapPin} label="Location" info="Toronto, Canada" />
 
-        <div className="flex items-center gap-2">
-          <MapPin strokeWidth={1.2} className="size-10 text-secondary-foreground" />
-          <div className="flex flex-col">
-            <label className="text-sm text-accent-foreground">Post date</label>
-            <p className="-mt-1 font-medium">
-              {post.date.getDate()}/{post.date.getMonth()}/{post.date.getFullYear()}
-            </p>
-          </div>
-        </div>
+        <QuickInfoItem
+          icon={Calendar}
+          label="Post date"
+          info={`${post.date.getDate()}/${post.date.getMonth()}/${post.date.getFullYear()}`}
+        />
       </section>
     </>
+  );
+}
+
+function QuickInfoItem({
+  icon,
+  label,
+  info
+}: {
+  icon: ComponentType<SVGProps<SVGSVGElement>>;
+  label: string;
+  info: string;
+}) {
+  const Icon = icon;
+
+  return (
+    <div className="flex items-center gap-2">
+      <Icon strokeWidth={1.2} className="size-8 text-secondary-foreground" />
+      <div className="flex flex-col">
+        <label className="text-sm text-accent-foreground">{label}</label>
+        <p className="tracking-tight -mt-1 font-medium">{info}</p>
+      </div>
+    </div>
   );
 }
