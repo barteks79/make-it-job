b623966a7c3450520589daada1d6e2696eb9fb4d
bartekvs08_ <sade79dev@gmail.com>
2025-08-12 11:56:15 +0200

    feat: change bg color of JobTagBadge

--- src/app/(core)/offers/_components/job-card/job-tag-badge.tsx
+++ src/app/(core)/offers/_components/job-card/job-tag-badge.tsx
@@ -1,16 +1,11 @@
 import { Badge } from '@/components/ui/badge';
 
-export default function JobTagBadge({
-	children
-}: React.PropsWithChildren) {
-	return (
-		<li>
-			<Badge
-				variant="outline"
-				className="bg-background text-secondary-foreground font-normal"
-			>
-				{children}
-			</Badge>
-		</li>
-	);
+export default function JobTagBadge({ children }: React.PropsWithChildren) {
+  return (
+    <li>
+      <Badge variant="outline" className="bg-accent text-accent-foreground font-normal">
+        {children}
+      </Badge>
+    </li>
+  );
 }
