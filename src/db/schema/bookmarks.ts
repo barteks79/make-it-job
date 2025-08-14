7922a8cc50008e8ac30e8073637aab56e0c41f0a
bartekvs08_ <sade79dev@gmail.com>
2025-08-14 14:16:28 +0200

    feat: create accounts, bookmarks, comapnies, posts schemas

---
+++ src/db/schema/bookmarks.ts
@@ -0,0 +1,16 @@
+import { pgTable, uuid } from 'drizzle-orm/pg-core';
+import { timestamps } from './timestamps.helpers';
+
+import { users } from './users';
+import { posts } from './posts';
+
+export const bookmarks = pgTable('bookmark', {
+  id: uuid('id').primaryKey(),
+  userId: uuid('userId')
+    .references(() => users.id, { onDelete: 'cascade', onUpdate: 'cascade' })
+    .notNull(),
+  postId: uuid('postId')
+    .references(() => posts.id, { onDelete: 'cascade', onUpdate: 'cascade' })
+    .notNull(),
+  ...timestamps
+});
---
+++ src/db/schema/companies.ts
@@ -0,0 +1,12 @@
+import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
+import { timestamps } from './timestamps.helpers';
+import { users } from './users';
+
+export const companies = pgTable('company', {
+  id: uuid('id').primaryKey(),
+  name: varchar('name', { length: 50 }).notNull().unique(),
+  ownerId: uuid('ownerId').references(() => users.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
+  description: varchar('description', { length: 255 }),
+  image: varchar('image', { length: 50 }),
+  ...timestamps
+});
