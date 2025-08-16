a637086a28dedfd8cf29675c9b87cc068c0ff416
bartekvs08_ <sade79dev@gmail.com>
2025-08-16 20:16:08 +0200

    feat: create getPostById query function

---
+++ src/db/queries/posts/get-post-by-id.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import { db } from '@/db';
+import { posts } from '@/db/schema/posts';
+import { companies } from '@/db/schema/companies';
+
+import { eq, type InferSelectModel } from 'drizzle-orm';
+import { unstable_cache } from 'next/cache';
+
+type JobPostWithCompany = {
+  post: InferSelectModel<typeof posts>;
+  company: InferSelectModel<typeof companies>;
+};
+
+export const getPostById = unstable_cache(
+  async (postId: string): Promise<JobPostWithCompany | undefined> => {
+    const [jobPost] = await db
+      .select()
+      .from(posts)
+      .innerJoin(companies, eq(posts.companyId, companies.id))
+      .where(eq(posts.id, postId));
+
+    return jobPost;
+  },
+  ['getPostById'],
+  { revalidate: 3600 } // 1 hour
+);
