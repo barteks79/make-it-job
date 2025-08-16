458ba6d6fde25bc3d42c437b29f2780bcf456e0c
bartekvs08_ <sade79dev@gmail.com>
2025-08-16 23:59:13 +0200

    feat: add Supabase client setups and image URL fetching functionality

---
+++ src/lib/supabase/get-image-url.ts
@@ -0,0 +1,18 @@
+import { unstable_cache } from 'next/cache';
+import { createClient } from './client';
+
+const client = createClient();
+
+// low-level fetcher (not cached)
+async function fetchImageUrl(path: string) {
+  console.log(`Fetching IMAGE: ${path}`);
+  const { data, error } = await client.storage.from('make-it-job').createSignedUrl(path, 3600);
+
+  if (error) throw error;
+  return data.signedUrl;
+}
+
+// wrapper that caches per path
+export function getImageUrl(path: string) {
+  return unstable_cache(() => fetchImageUrl(path), ['getImageUrl', path], { revalidate: 3600 })();
+}
