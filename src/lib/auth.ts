191993cf44f3758aedbb93e25ceaa91ea16acbb4
bartekvs08_ <sade79dev@gmail.com>
2025-08-23 15:21:05 +0200

    feat: add better-auth configuration

---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '@/db';
+
+export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  url: process.env.BETTER_AUTH_URL,
+  database: drizzleAdapter(db, {
+    provider: 'pg'
+  }),
+  emailAndPassword: {
+    enabled: true
+  }
+});
