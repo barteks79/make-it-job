94dd61acc19cab09d43dc91dc902124404a574d1
bartekvs08_ <sade79dev@gmail.com>
2025-08-24 13:38:07 +0200

    fix: change field name to emailVerified

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -6,7 +6,7 @@ export const users = pgTable('user', {
   id: uuid('id').primaryKey(),
   name: varchar('name', { length: 50 }).notNull(),
   email: varchar('email', { length: 100 }).unique().notNull(),
-  emailVerified: boolean('emailVerification').notNull(),
+  emailVerified: boolean('emailVerified').notNull(),
   image: varchar('image', { length: 50 }).unique(),
   ...timestamps
 });
