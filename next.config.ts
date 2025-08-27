6e346070335af2964e6c6d400493af060aa0e9f7
bartekvs08_ <sade79dev@gmail.com>
2025-08-27 22:11:19 +0200

    chore: update next config for image remote patterns; increase Supabase connection limits

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,33 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	eslint: {
-		ignoreDuringBuilds: true
-	}
+  // config options here
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb'
+    }
+  },
+  images: {
+    remotePatterns: [
+      {
+        // local supabase
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '54321',
+        pathname: '/storage/v1/object/sign/make-it-job/**'
+      },
+      {
+        // github avatars
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com'
+      },
+      {
+        // google avatars
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
