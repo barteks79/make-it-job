f287ab00454909d9919b403fa6e919c77e46ebab
bartekvs08_ <sade79dev@gmail.com>
2025-09-05 21:05:14 +0200

    feat: add custom logger to drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,4 +5,20 @@ const databaseUrl = process.env.DATABASE_URL;
 if (!databaseUrl) throw new Error('Database URL is not set.');
 
 const client = postgres(databaseUrl, { prepare: false });
-export const db = drizzle(client);
+export const db = drizzle(client, {
+  logger:
+    process.env.NODE_ENV === 'development'
+      ? {
+          logQuery(query: string, params: unknown[]) {
+            console.log('='.repeat(60));
+
+            console.log('\n\x1b[32m[Drizzle]\x1b[0m\n');
+
+            console.log(`Query: ${query}\n`);
+            if (params && params.length > 0) {
+              console.log(`Params: ${JSON.stringify(params, null, 2)}\n`);
+            }
+          }
+        }
+      : undefined
+});
