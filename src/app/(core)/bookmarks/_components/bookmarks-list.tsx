3d53567336885f346df04058884e9187f97c2264
Bartosz Sarnowski <sade79dev@gmail.com>
2025-10-09 14:48:19 +0200

    feat: implement responsive design to BookmarksListItem

--- src/app/(core)/bookmarks/_components/bookmarks-list.tsx
+++ src/app/(core)/bookmarks/_components/bookmarks-list.tsx
@@ -6,6 +6,7 @@ import { type GetUserBookmarks } from '@/db/queries/bookmarks/get-user-bookmarks
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardAction } from '@/components/ui/card';
+import { Send } from 'lucide-react';
 import BookmarkButton from '../../offers/_components/job-card/bookmark-button';
 import { createRelativeDate } from '@/lib/utils';
 
@@ -20,27 +21,30 @@ function BookmarkListItem({ bookmark, onRemove }: BookmarkListItemProps) {
       <Card className="py-3">
         <CardContent className="flex justify-between px-3">
           <div className="flex items-center gap-3.5">
-            <div className="relative size-16">
+            <div className="relative size-12 min-w-12 lg:size-16 lg:min-w-16">
               <Image className="rounded" src="/images/user-default1.jpg" alt="User" fill />
             </div>
 
             <div className="flex flex-col">
-              <h3 className="font-semibold text-lg tracking-tight">
+              <h3 className="font-semibold text-base lg:text-lg tracking-tight line-clamp-1">
                 {bookmark.post.experience} {bookmark.post.position}, Netflix
               </h3>
-              <p className="text-muted-foreground tracking-tight line-clamp-1">
+              <p className="text-muted-foreground tracking-tight line-clamp-1 text-sm lg:text-base">
                 {bookmark.post.description}
               </p>
             </div>
           </div>
 
-          <CardAction className="flex flex-col justify-between">
-            <p className="text-muted-foreground tracking-tight text-right">
+          <CardAction className="flex flex-col justify-between ">
+            <div className="text-muted-foreground tracking-tight line-clamp-1">
               {createRelativeDate(new Date(bookmark.post.createdAt))}
-            </p>
+            </div>
 
             <div className="flex gap-1">
-              <Button className="h-8 px-8">Apply</Button>
+              <Button className="w-8 md:w-auto h-8 px-2 md:px-8">
+                <Send className="size-4 md:hidden" />
+                <span className="hidden md:inline">Apply</span>
+              </Button>
 
               <BookmarkButton
                 updatedParentUI={() => onRemove(bookmark.post.id)}
