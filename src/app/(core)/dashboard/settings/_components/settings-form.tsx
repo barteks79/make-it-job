4945232a37cb9d3b8865bec5d3295f7a78149027
bartekvs08_ <sade79dev@gmail.com>
2025-10-10 19:51:14 +0200

    feat: refactor SettingsForm

--- src/app/(core)/dashboard/settings/_components/settings-form.tsx
+++ src/app/(core)/dashboard/settings/_components/settings-form.tsx
@@ -1,79 +1,55 @@
 'use client';
 
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { toast } from 'sonner';
-
 import { authClient } from '@/lib/auth/client';
-import { updateSettings } from '@/app/(core)/dashboard/settings/_actions/update-settings';
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage
-} from '@/components/ui/form';
+import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { SettingsFormSkeleton } from './skeletons';
+import { Badge } from '@/components/ui/badge';
 
-import { settingsSchema, type TSettings } from '@/types/settings-schema';
+import { SettingsFormSkeleton } from './skeletons';
+import { InboxIcon, LockKeyholeIcon } from 'lucide-react';
 
 export function SettingsForm() {
   const { data: auth, isPending } = authClient.useSession();
-
-  const form = useForm<TSettings>({
-    resolver: zodResolver(settingsSchema),
-    defaultValues: {
-      email: '',
-      password: ''
-    }
-  });
-
-  async function onSubmit() {
-    const result = await updateSettings();
-
-    if (result.success) toast.success(result.message);
-    else toast.error(result.message);
-  }
 
   if (isPending || !auth) {
     return <SettingsFormSkeleton />;
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="max-w-96 space-y-4">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="jkowalski@email.com" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="********" type="password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Update settings</Button>
-      </form>
-    </Form>
+    <form className="space-y-4">
+      <div className="space-y-1.5">
+        <Label className="text-base">Email</Label>
+
+        <div className="flex items-center gap-2">
+          <Input value={auth.user.email} disabled />
+          <Button className="space-x-0.5 cursor-pointer" variant="secondary">
+            <span>Change Email</span>
+            <InboxIcon />
+          </Button>
+        </div>
+
+        {auth.user.emailVerified ? (
+          <Badge className="bg-destructive/25 text-destructive font-normal cursor-default">
+            Not verified
+          </Badge>
+        ) : (
+          <Badge className="bg-primary/25 text-primary font-normal cursor-default">Verified</Badge>
+        )}
+      </div>
+
+      <div className="space-y-1.5">
+        <Label className="text-base">Password</Label>
+
+        <div className="flex items-center gap-2">
+          <Input disabled placeholder="********************" type="password" />
+          <Button className="space-x-0.5 cursor-pointer" variant="secondary">
+            <span>Change Password</span>
+            <LockKeyholeIcon />
+          </Button>
+        </div>
+      </div>
+    </form>
   );
 }
