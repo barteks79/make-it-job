6679ccc78d408b5dcba90f0a5d9f639c53bac30d
bartekvs08_ <sade79dev@gmail.com>
2025-10-11 17:15:45 +0200

    feat: update SettingsFormSkeleton

--- src/app/(core)/dashboard/settings/_components/skeletons.tsx
+++ src/app/(core)/dashboard/settings/_components/skeletons.tsx
@@ -19,6 +19,48 @@ export function SettingsFormSkeleton() {
           <Skeleton className="h-9 w-60" />
         </div>
       </div>
+
+      <div className="space-y-2.5">
+        <section className="space-y-1">
+          <Skeleton className="h-6 w-32" />
+          <Skeleton className="h-4 w-full" />
+        </section>
+
+        <section className="border rounded-md">
+          <div className="flex items-center justify-between py-4 px-5">
+            <div className="flex items-center gap-4">
+              <Skeleton className="size-5 rounded" />
+              <div className="flex flex-col gap-1">
+                <Skeleton className="h-4 w-24" />
+                <Skeleton className="h-3 w-40" />
+              </div>
+            </div>
+            <Skeleton className="h-9 w-20" />
+          </div>
+
+          <div className="flex items-center justify-between py-4 px-5 border-y">
+            <div className="flex items-center gap-4">
+              <Skeleton className="size-5 rounded" />
+              <div className="flex flex-col gap-1">
+                <Skeleton className="h-4 w-16" />
+                <Skeleton className="h-3 w-32" />
+              </div>
+            </div>
+            <Skeleton className="h-9 w-20" />
+          </div>
+
+          <div className="flex items-center justify-between py-4 px-5">
+            <div className="flex items-center gap-4">
+              <Skeleton className="size-5 rounded" />
+              <div className="flex flex-col gap-1">
+                <Skeleton className="h-4 w-16" />
+                <Skeleton className="h-3 w-32" />
+              </div>
+            </div>
+            <Skeleton className="h-9 w-20" />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
