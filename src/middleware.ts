aefd93cdfbd25bf30f0c3d77b946167fbf1636d3
bartekvs08_ <sade79dev@gmail.com>
2025-10-17 19:51:12 +0200

    fix: remove auth calls from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,10 @@
 import { NextResponse, type NextRequest } from 'next/server';
 import { getSessionCookie } from 'better-auth/cookies';
 
-import { authClient } from '@/lib/auth/client';
-
-const authRoutes = ['/sign-in', '/sign-up'];
 const privateRoutes = ['/dashboard', '/bookmarks'];
 
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const session = getSessionCookie(request);
-
-  // log out if trying to access auth routes while logged in
-  if (session && authRoutes.includes(request.nextUrl.pathname)) {
-    await authClient.revokeSession({ token: session });
-    return NextResponse.next();
-  }
 
   // redirect to "/sign-in" if not authorized
   if (!session && privateRoutes.some(route => request.nextUrl.pathname.startsWith(route))) {
