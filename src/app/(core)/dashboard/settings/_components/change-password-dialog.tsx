b1c4ac69c26f481779d93b45a9117e3295414874
bartekvs08_ <sade79dev@gmail.com>
2025-12-23 16:16:54 +0100

    feat: add password change success state

--- src/app/(core)/dashboard/settings/_components/change-password-dialog.tsx
+++ src/app/(core)/dashboard/settings/_components/change-password-dialog.tsx
@@ -29,12 +29,13 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Spinner } from '@/components/ui/spinner';
-import { AlertCircleIcon } from 'lucide-react';
+import { AlertCircleIcon, CheckCircle2 } from 'lucide-react';
 
 import { changePasswordSchema, type TChangePassword } from '@/types/change-password-schema';
 
 export function ChangePasswordDialog({ children }: React.PropsWithChildren) {
-  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(true);
+  const [isSuccess, setIsSuccess] = useState<boolean>(true);
 
   const form = useForm<TChangePassword>({
     resolver: zodResolver(changePasswordSchema),
@@ -51,6 +52,10 @@ export function ChangePasswordDialog({ children }: React.PropsWithChildren) {
       newPassword,
       revokeOtherSessions: true,
       fetchOptions: {
+        onSuccess: () => {
+          setIsSuccess(true);
+          form.reset();
+        },
         onError: async context => {
           const { response } = context;
           if (response.status === 429) {
@@ -70,91 +75,117 @@ export function ChangePasswordDialog({ children }: React.PropsWithChildren) {
   const handleOpenChange = (open: boolean) => {
     form.reset();
     setIsDialogOpen(open);
+    setIsSuccess(false);
   };
 
   return (
     <Dialog open={isDialogOpen} onOpenChange={handleOpenChange}>
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent>
-        <DialogHeader className="gap-1">
-          <DialogTitle className="text-xl">Change Password</DialogTitle>
-          <DialogDescription className="text-base">
-            Enter your new password below.
-          </DialogDescription>
-        </DialogHeader>
-
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="currentPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Current Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter your current password" type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="newPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>New Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter your new password" type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="confirmNewPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Confirm New Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Confirm your new password" type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            {form.formState.errors.root && (
-              <Alert variant="destructive">
-                <AlertCircleIcon className="relative top-2" />
-                <AlertTitle>Unexpected error occurred.</AlertTitle>
-                <AlertDescription>{form.formState.errors.root.message}</AlertDescription>
-              </Alert>
-            )}
-
+        {isSuccess ? (
+          <>
+            <DialogHeader className="items-center gap-1.5 pt-4">
+              <div className="flex items-center gap-2">
+                <CheckCircle2 className="size-6.5 text-primary" />
+                <DialogTitle className="text-xl">Password Changed</DialogTitle>
+              </div>
+              <DialogDescription className="text-center text-base">
+                Your password has been successfully updated.
+              </DialogDescription>
+            </DialogHeader>
             <DialogFooter>
               <DialogClose asChild>
-                <Button type="button" variant="outline">
-                  Cancel
-                </Button>
+                <Button type="button">Close</Button>
               </DialogClose>
-
-              <Button disabled={form.formState.isSubmitting} type="submit">
-                {form.formState.isSubmitting ? (
-                  <>
-                    Changing...
-                    <Spinner />
-                  </>
-                ) : (
-                  'Change Password'
-                )}
-              </Button>
             </DialogFooter>
-          </form>
-        </Form>
+          </>
+        ) : (
+          <>
+            <DialogHeader className="gap-1">
+              <DialogTitle className="text-xl">Change Password</DialogTitle>
+              <DialogDescription className="text-base">
+                Enter your new password below.
+              </DialogDescription>
+            </DialogHeader>
+
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="currentPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Current Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your current password"
+                          type="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="newPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>New Password</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your new password" type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="confirmNewPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Confirm New Password</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Confirm your new password" type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {form.formState.errors.root && (
+                  <Alert variant="destructive">
+                    <AlertCircleIcon className="relative top-2" />
+                    <AlertTitle>Unexpected error occurred.</AlertTitle>
+                    <AlertDescription>{form.formState.errors.root.message}</AlertDescription>
+                  </Alert>
+                )}
+
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button type="button" variant="outline">
+                      Cancel
+                    </Button>
+                  </DialogClose>
+
+                  <Button disabled={form.formState.isSubmitting} type="submit">
+                    {form.formState.isSubmitting ? (
+                      <>
+                        Changing...
+                        <Spinner />
+                      </>
+                    ) : (
+                      'Change Password'
+                    )}
+                  </Button>
+                </DialogFooter>
+              </form>
+            </Form>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
